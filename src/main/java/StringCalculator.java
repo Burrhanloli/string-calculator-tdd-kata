013aac2306bbfc05248354225ce112c338447aa1
burhanuddin <burhanuddin.l@goevive.com>
2021-02-15 12:44:25 +0530

    added delimeter testcases

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,18 @@
 import com.google.common.base.Strings;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StringCalculator {
+
+  private String delimiter;
+  private String numbers;
+
+  private StringCalculator(String delimiter, String numbers) {
+    this.delimiter = delimiter;
+    this.numbers = numbers;
+  }
+
 
   public static void main(String[] args) {
 
@@ -11,10 +22,26 @@ public class StringCalculator {
     if (Strings.isNullOrEmpty(numbers)) {
       return 0;
     } else {
-      return Stream.of(numbers.split(","))
-          .filter(i -> !Strings.isNullOrEmpty(i) || "null".equalsIgnoreCase(i))
+      StringCalculator calculator = parseInput(numbers);
+      return Stream.of(calculator.numbers.split(calculator.delimiter))
+          .filter(i -> !Strings.isNullOrEmpty(i))
+          .filter(i -> !"null".equalsIgnoreCase(i))
           .mapToInt(Integer::parseInt)
           .sum();
     }
+  }
+
+  private static StringCalculator parseInput(String input) {
+    if (input.startsWith("//")) {
+      String[] headerAndNumberSequence = input.split("\n", 2);
+      String delimiter = parseDelimiter(headerAndNumberSequence[0]);
+      return new StringCalculator(delimiter, headerAndNumberSequence[1]);
+    } else {
+      return new StringCalculator(",|\n", input);
+    }
+  }
+
+  private static String parseDelimiter(String header) {
+    return header.substring(2);
   }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -27,4 +27,38 @@ public class StringCalculatorTest {
   public void addThreeNumbersSeparatedByComma() {
     assertThat(StringCalculator.Add("1,2,4"), is(7));
   }
+
+  @Test
+  public void addNumbersSeparatedByCommaWithNullString() {
+    assertThat(StringCalculator.Add("1,null"), is(1));
+    assertThat(StringCalculator.Add("1,3,null"), is(4));
+  }
+
+  @Test
+  public void addNumbersSeparatedByCommaWithEmptyString() {
+    assertThat(StringCalculator.Add("1,"), is(1));
+    assertThat(StringCalculator.Add("1,3,"), is(4));
+  }
+
+  @Test
+  public void addNullTo0() {
+    assertThat(StringCalculator.Add(null), is(0));
+  }
+
+  @Test
+  public void addNumbersDelimitedByNewline() {
+    assertThat(StringCalculator.Add("1\n10"), is(11));
+  }
+
+  @Test
+  public void addNumbersDelimitedByCommaOrNewline() {
+    assertThat(StringCalculator.Add("1,21\n30"), is(52));
+  }
+
+  @Test
+  public void usesDelimiterSepcified() {
+    assertThat(StringCalculator.Add("//;\n1;40"), is(41));
+    assertThat(StringCalculator.Add("//:\n2:3:4:1"), is(10));
+  }
+
 }
